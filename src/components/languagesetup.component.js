0fa8124363200b8ab0fc7cba31dd0f74ccba0d46
zivoo <zivodvok@gmail.com>
2020-09-20 10:41:20 +0300

    removed login.css reference

--- src/components/languagesetup.component.js
+++ src/components/languagesetup.component.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import './Login.css';
 import Language from "./language.component";
 import Languagedit from "./Languagedit.component";
 
