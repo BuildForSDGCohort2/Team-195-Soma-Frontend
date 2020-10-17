16ec834810bf382218d287c82e5068b7a98c3a10
Habiba Mohammed <m.habiba@vivatelecoms.com>
2020-10-18 01:10:42 +0300

    Add landing page

--- src/components/about.component.js
+++ src/components/about.component.js
@@ -5,107 +5,166 @@ export default class About extends Component {
     render() {
         return (
             <div>
-        <section className="colorlib-about" data-section="about">
-        <div className="colorlib-narrow-content">
-            <div className="row">
-            <div className="col-md-12">
-                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
-                <div className="col-md-12">
-                    <div className="about-desc">
-                    <span className="heading-meta">About Us</span>
-                    <h2 className="colorlib-heading">WHO WE ARE?</h2>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. )</p>
-                    <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-!</p>
-                    </div>
+        
+       < div id="container" className="container-page" style={{width: "100%",marginLeft:"200px"}}>
+            <div className="myToolbar">
+                <div className="myMenu clickable">
+                    <img src="/img/icons/menu.png" alt="menu icon" className="myIcon" onClick={this.drawerState}/>
                 </div>
-                </div>
+                <h3>Courses and Lessons</h3>
             </div>
+            <div id="topic">
+            
             </div>
-        </div>
-        </section>
-        <section className="colorlib-about">
-        <div className="colorlib-narrow-content">
-            <div className="row">
-            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+            <div className="content-wrapper">
                 
-                <h2 className="colorlib-heading">WHAT WE DO</h2>
-            </div>
-            </div>
-            <div className="row row-pt-md">
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-1">
-                <span className="icon">
-                    <i className="icon-bulb" />
-                </span>
-                <div className="desc">
-                    <h3>BASIC READING AND WRITTING </h3>
-                    <p>is simply dummy text of the printing and typesetting industry</p>
-                </div>
-                </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-3">
-                <span className="icon">
-                    <i className="icon-phone3" />
-                </span>
-                <div className="desc">
-                    <h3>MATHS SKILLS</h3>
-                    <p>is simply dummy text of the printing and typesetting industry</p>
-                </div>
-                </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-5">
-                <span className="icon">
-                    <i className="icon-data" />
-                </span>
-                <div className="desc">
-                    <h3>MATHS SKILLS</h3>
-                    <p>is simply dummy text of the printing and typesetting industry</p>
-                </div>
-                </div>
-            </div>
-            {/*
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-2">
-                <span className="icon">
-                    <i className="icon-data" />
-                </span>
-                <div className="desc">
-                    <h3>Dev Ops</h3>
-                    <p>Jenkins , Kubernetes , Docker </p>
-                </div>
-                </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-4">
-                <span className="icon">
-                    <i className="icon-layers2" />
-                </span>
-                <div className="desc">
-                    <h3>Graphic Design</h3>
-                    <p>My friend knows .. P</p>
-                </div>
-                </div>
-            </div>
-            <div className="col-md-4 text-center animate-box">
-                <div className="services color-6">
-                <span className="icon">
-                    <i className="icon-phone3" />
-                </span>
-                <div className="desc">
-                    <h3>Digital Marketing</h3>
-                    <p>I use Instagram eight hours a day :) </p>
-                </div>
-                </div>
-            </div>
-            */}
-            </div>
+                    <div className="course-body" >
+                    <section className="section-padding">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-2 col-sm-12 col-xs-12">
+          <div className="text-center wow fadeIn">
+            <h2 className="xs-font20">Welcome to Soma</h2>
+            <p>Our app provides first step to self and continuous learning process. It gives entry point to illiterate people. With latest challenges in the world like corona Virus everything is going online, but illiterate people can’t improve their lives or find resource to face the challenges. The app will allow people to learn on their own and reduce the chances of being looked down at. The app provides voice and video lessons and writing illustration. We also plan including the ability to connect learners and teachers.</p>
+          </div>
         </div>
-        </section>
       </div>
+    </div>
+  </section>
+    <section className="features-top-area" id="features">
+  <div className="container">
+    <div className="row promo-content">
+      <div className="col-md-4 col-lg-4 col-sm-6 col-xs-12">
+        <div className="text-icon-box mb20 xs-mb0 wow fadeInUp padding30" data-wow-delay="0.1s">
+          <div className="box-icon features-box-icon">
+            <i className="fa fa-graduation-cap" />
+          </div>
+          <h3 className="box-title">Learn the Basics</h3>
+          <p>A Google Docs scam that appears to be widespread began landing in Wednesday in what seemed to be a phishing attack.</p>
+        </div>
+      </div>
+      <div className="col-md-4 col-lg-4 col-sm-6 col-xs-12">
+        <div className="text-icon-box relative mb20 xs-mb0  wow fadeInUp padding30" data-wow-delay="0.2s">
+          <div className="box-icon features-box-icon">
+            <i className="icofont icofont-business-man-alt-1" />
+          </div>
+          <h3 className="box-title">Connect with Expert Instructors</h3>
+          <p>A Google Docs scam that appears to be widespread began landing in Wednesday in what seemed to be a phishing attack.</p>
+        </div>
+      </div>
+      <div className="col-md-4 col-lg-4 col-sm-6 col-xs-12">
+        <div className="text-icon-box relative mb20 xs-mb0 wow fadeInUp padding30" data-wow-delay="0.3s">
+          <div className="box-icon features-box-icon">
+            <i className="fa fa-rocket" />
+          </div>
+          <h3 className="box-title">Learn from anywhere</h3>
+          <p>A Google Docs scam that appears to be widespread began landing in Wednesday in what seemed to be a phishing attack.</p>
+        </div>
+      </div>
+      <div className="col-md-4 col-lg-4 col-sm-6 col-xs-12">
+        <div className="text-icon-box relative mb20 xs-mb0 wow fadeInUp padding30 visible-sm" data-wow-delay="0.4s">
+          <div className="box-icon features-box-icon">
+            <i className="fa fa-cog" />
+          </div>
+          <h3 className="box-title">Quality Service</h3>
+          <p>A Google Docs scam that appears to be widespread began landing in Wednesday in what seemed to be a phishing attack.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+                <div>
+  
+  {/*ABOUT TOP CONTENT AREA END*/}
+ 
+  {/*COURSE AREA*/}
+  <section className="course-area padding-top" id="courses">
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-2 col-sm-12 col-xs-12">
+          <div className="area-title text-center wow fadeIn">
+            <h2 className="xs-font26">Our Trending Courses</h2>
+          </div>
+        </div>
+      </div>
+      <div className="row course-list">
+        <div className="col-md-3 col-lg-3 col-sm-6 col-xs-12">
+          <div className="single-course mb20">
+            <img src="assest/img/course/blog-1.jpg" alt />
+            <div className="course-details padding30">
+              <h3 className="font18">How to Write a Powerful Speech</h3>
+              <p>MPs who are leaving the protection of for the campaign trail will render...</p>
+              <p className="mt30"><a href="#" className="enroll-button">Enroll the Courses</a> </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3 col-lg-3 col-sm-6 col-xs-12">
+          <div className="single-course mb20">
+            <img src="assest/img/course/blog-2.jpg" alt />
+            <div className="course-details padding30">
+              <h3 className="font18">The Art and Practice of Leadership</h3>
+              <p>MPs who are leaving the protection of for the campaign trail will render...</p>
+              <p className="mt30"><a href="#" className="enroll-button">Enroll the Courses</a> </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3 col-lg-3 col-sm-6 col-xs-12">
+          <div className="single-course mb20">
+            <img src="assest/img/course/blog-3.jpg" alt />
+            <div className="course-details padding30">
+              <h3 className="font18">Introduction to Resume Writing</h3>
+              <p>MPs who are leaving the protection of for the campaign trail will render...</p>
+              <p className="mt30"><a href="#" className="enroll-button">Enroll the Courses</a> </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3 col-lg-3 col-sm-6 col-xs-12">
+          <div className="single-course mb20">
+            <img src="assest/img/course/blog-4.jpg" alt />
+            <div className="course-details padding30">
+              <h3 className="font18">The Art and Practice of Leadership</h3>
+              <p>MPs who are leaving the protection of for the campaign trail will render...</p>
+              <p className="mt30"><a href="#" className="enroll-button">Enroll the Courses</a> </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  {/*COURSE AREA END*/}
+  {/*ABOUT AREA*/}
+  <section className="section-padding about-area" id="about">
+    <div className="container">
+      <div className="row flex-v-center">
+        <div className="col-md-5 col-lg-5 col-sm-12 col-xs-12">
+          <div className="about-content xs-mb50 wow fadeIn">
+            <h3 className="xs-font20 mb30">Education is the foundation upon which we build our future.</h3>
+            <p>A Google Docs scam that appears to be widespread began landing in users’ inboxes on Wednesday in what seemed to be a sophisticated phishing or malware attack. The deceptive invitation to edit a Google Doc on Google Docs with you.</p>
+          </div>
+        </div>
+        <div className="col-md-6 col-lg-6 col-md-offset-1 col-lg-offset-1 col-sm-12 col-xs-12">
+          <div className="video-promo-details wow fadeIn">
+            <div className="video-promo-content">
+              <span data-video-id="j1S66liv1t8" className="video-area-popup mb30"><i className="fa fa-play" /></span>
+            </div>
+            <img src="assest/img/about/about.jpg" alt />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  {/*ABOUT AREA END*/}
+</div>
+
+
+                    </div>
+               
+            </div>
+            </div>
+      </div>
+      
             
             
         );
