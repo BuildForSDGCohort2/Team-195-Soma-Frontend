2251e51451706db4855248b6c48c7e00c9b135da
Habiba Mohammed <m.habiba@vivatelecoms.com>
2020-10-20 22:05:14 +0300

    add styles to landing page

--- src/components/about.component.js
+++ src/components/about.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect,Link } from "react-router-dom";
 
 
 export default class About extends Component {
@@ -6,12 +7,49 @@ render() {
 return (
 <div>
 
-< div id="container" className="container-page" style={{width: "100%",marginLeft:"200px"}}>
+< div id="container" className="container-main-page" >
 <div className="myToolbar">
-<div className="myMenu clickable">
-<img src="/img/icons/menu.png" alt="menu icon" className="myIcon" onClick={this.drawerState}/>
-</div>
-<h3>Courses and Lessons</h3>
+
+<nav className="navbar">
+            <div className="container">
+              <Link className="navbar-brand" to={"/"}>
+                Soma
+              </Link>
+
+              <div
+                className="collapse navbar-collapse"
+                id="navbarTogglerDemo02"
+              >
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/about"}>
+                      About Soma
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/courses"}>
+                      Courses
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/sign-in"}>
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={"/sign-up"}>
+                      Sign up
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="main">
+
+          <div className="page">
+           </div>
+          </div>
+          </nav>
 </div>
 <div id="topic">
 
@@ -79,83 +117,7 @@ return (
 
 {/*ABOUT TOP CONTENT AREA END*/}
 
-{/*COURSE AREA*/}
-<section className="course-area padding-top" id="courses">
-<div className="container-fluid">
-<div className="row">
-<div className="col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-2 col-sm-12 col-xs-12">
-<div className="area-title text-center wow fadeIn">
-<h2 className="xs-font26">Our Trending Courses</h2>
-</div>
-</div>
-</div>
-<div className="row course-list">
-<div className="col-md-3 col-lg-3 col-sm-6 col-xs-12">
-<div className="single-course mb20">
-<img src="assest/img/course/blog-1.jpg" alt />
-<div className="course-details padding30">
-<h3 className="font18">How to Write a Powerful Speech</h3>
-<p>MPs who are leaving the protection of for the campaign trail will render...</p>
-<p className="mt30"><a href="#" className="enroll-button">Enroll the Courses</a> </p>
-</div>
-</div>
-</div>
-<div className="col-md-3 col-lg-3 col-sm-6 col-xs-12">
-<div className="single-course mb20">
-<img src="assest/img/course/blog-2.jpg" alt />
-<div className="course-details padding30">
-<h3 className="font18">The Art and Practice of Leadership</h3>
-<p>MPs who are leaving the protection of for the campaign trail will render...</p>
-<p className="mt30"><a href="#" className="enroll-button">Enroll the Courses</a> </p>
-</div>
-</div>
-</div>
-<div className="col-md-3 col-lg-3 col-sm-6 col-xs-12">
-<div className="single-course mb20">
-<img src="assest/img/course/blog-3.jpg" alt />
-<div className="course-details padding30">
-<h3 className="font18">Introduction to Resume Writing</h3>
-<p>MPs who are leaving the protection of for the campaign trail will render...</p>
-<p className="mt30"><a href="#" className="enroll-button">Enroll the Courses</a> </p>
-</div>
-</div>
-</div>
-<div className="col-md-3 col-lg-3 col-sm-6 col-xs-12">
-<div className="single-course mb20">
-<img src="assest/img/course/blog-4.jpg" alt />
-<div className="course-details padding30">
-<h3 className="font18">The Art and Practice of Leadership</h3>
-<p>MPs who are leaving the protection of for the campaign trail will render...</p>
-<p className="mt30"><a href="#" className="enroll-button">Enroll the Courses</a> </p>
-</div>
-</div>
-</div>
-</div>
-</div>
-</section>
-{/*COURSE AREA END*/}
-{/*ABOUT AREA*/}
-<section className="section-padding about-area" id="about">
-<div className="container">
-<div className="row flex-v-center">
-<div className="col-md-5 col-lg-5 col-sm-12 col-xs-12">
-<div className="about-content xs-mb50 wow fadeIn">
-<h3 className="xs-font20 mb30">Education is the foundation upon which we build our future.</h3>
-<p>A Google Docs scam that appears to be widespread began landing in users’ inboxes on Wednesday in what seemed to be a sophisticated phishing or malware attack. The deceptive invitation to edit a Google Doc on Google Docs with you.</p>
-</div>
-</div>
-<div className="col-md-6 col-lg-6 col-md-offset-1 col-lg-offset-1 col-sm-12 col-xs-12">
-<div className="video-promo-details wow fadeIn">
-<div className="video-promo-content">
-<span data-video-id="j1S66liv1t8" className="video-area-popup mb30"><i className="fa fa-play" /></span>
-</div>
-<img src="assest/img/about/about.jpg" alt />
-</div>
-</div>
-</div>
-</div>
-</section>
-{/*ABOUT AREA END*/}
+
 </div>
 
 
